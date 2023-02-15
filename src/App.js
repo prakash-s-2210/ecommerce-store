601aa6dbc61f5842c95e05b8bb8f413379ceb634
Prakash <prakash.s@disprz.com>
2023-02-15 19:51:59 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Header from './containers/Header';
+import ProductListing from './containers/ProductListing';
+import ProductDetail from './containers/ProductDetail';
+import {BrowserRouter} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route  path = "/" element = {<ProductListing />}></Route>
+        <Route  path = "/product/:productId" element = {<ProductDetail />}></Route>
+        <Route>404 NOT fOUND!</Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
